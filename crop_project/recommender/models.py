afeac2b5c36bb9dd678a8959a0cb4f95886d24d6
Maulik Patel <maulikcmr05@gmail.com>
2026-04-25 21:53:52 +0530

    '25-4'

--- crop_project/recommender/models.py
+++ crop_project/recommender/models.py
@@ -11,10 +11,19 @@ class UserProfile(models.Model):
         return self.user.get_full_name() or self.user.username
     
 class Prediction(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE,reversed_name='predictions')
-    crop_name = models.CharField(max_length=100)
-    predicted_yield = models.FloatField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='predictions')
+    N = models.FloatField()
+    P = models.FloatField()
+    K = models.FloatField()
+    temperature = models.FloatField()
+    humidity = models.FloatField()
+    ph = models.FloatField()
+    rainfall = models.FloatField()
+    predicted_label = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
-    
+
+    class Meta:
+        ordering = ['-created_at']
+
     def __str__(self):
-        return f"{self.crop_name} - {self.predicted_yield}"
+        return f"{self.user.username} -> {self.predicted_label}"
